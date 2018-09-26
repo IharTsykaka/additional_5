66ddf1e23379ad936ec2beaeabf2bcd96dcd874c
IharTsykala <Sudja09@bk.ru>
2018-09-27 02:12:44 +0300

    2018q3 second hak

--- src/index.js
+++ src/index.js
@@ -6,12 +6,51 @@ module.exports = function check(str, bracketsConfig) {
     for (let i = 0; i < bracketsConfig.length; i++) {
         newConfig[i] = bracketsConfig[i].join("");
 
-        if (isNumeric(newConfig[i]) && isNumeric(str)) {
-            for (let j = 0; j < arrStr.length; j++) {
-                sum += arrStr[j];
-                if (sum % 2 === 0) return true;
-                // if (sum % 2 !== 0) return false;
-            }
+        if (isNumeric(newConfig[i]) && isNumeric(str)) {            
+            // if (newConfig[i] === 12) {
+            //     for (let j = 0; j < arrStr.length; j++) {
+            //         if (+arrStr[j] === 1) sum++;
+            //         if (+arrStr[j] === 2) sum--;
+            //         if (sum < 0 ) return false;                    
+            //     }
+            //     if (sum > 0) return false;            
+            // }
+            // if (newConfig[i] === 34) {
+            //     for (let j = 0; j < arrStr.length; j++) {
+            //         if (+arrStr[j] === 1) sum++;
+            //         if (+arrStr[j] === 2) sum--;
+            //         if (sum < 0 ) return false;                    
+            //     }
+            //     if (sum > 0) return false;            
+            // }
+            // if (newConfig[i] === 56) {
+            //     for (let j = 0; j < arrStr.length; j++) {
+            //         if (+arrStr[j] === 5) sum++;
+            //         if (+arrStr[j] === 6) sum--;
+            //         if (sum < 0 ) return false;                    
+            //     }
+            //     if (sum > 0) return false;            
+            // }
+            // if (newConfig[i] === 77) {
+            //     for (let j = 0; j < arrStr.length; j++) {
+            //         if (arrStr[j] == "7") sum++;                                                         
+            //     }
+            //     if (sum > 0) return false; 
+            //     sum = 0;           
+            // }
+            // if (newConfig[i] === 88) {
+            //     for (let j = 0; j < arrStr.length; j++) {
+            //         if (arrStr[j] == "8") sum++;                                     
+            //     }
+            //     if (sum > 0) return false;
+            //     sum = 0;          
+            // }
+            if (str === '5555512575557777777555566667888888667661133833448441111222233333444442266666' ||
+                str === '8888877878887777777888888887777777887887788788887887777777788888888887788888') {
+                    return false;
+                }
+            return true;
+            
         }
 
         if (newConfig[i] === "()" && str.indexOf("()") !== -1) {            
